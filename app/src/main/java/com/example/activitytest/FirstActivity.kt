9876c67e6ecb401a3f8a903324ce705f499e785b
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-04-23 17:00:58 +0800

    Update FirstActivity.kt

--- app/src/main/java/com/example/activitytest/FirstActivity.kt
+++ app/src/main/java/com/example/activitytest/FirstActivity.kt
@@ -2,11 +2,17 @@ package com.example.activitytest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.first_layout.*
 
 class FirstActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.first_layout)
+        button1.setOnClickListener {
+            Toast.makeText(this,"You clicked Button 1", Toast.LENGTH_SHORT).show()
+        }
     }
 }
