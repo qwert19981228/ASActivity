8de82fbc5be10953b5403368f2cd1dde4cb43f0b
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-04-23 17:19:38 +0800

    Update FirstActivity.kt

--- app/src/main/java/com/example/activitytest/FirstActivity.kt
+++ app/src/main/java/com/example/activitytest/FirstActivity.kt
@@ -1,7 +1,10 @@
 package com.example.activitytest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.first_layout.*
@@ -11,8 +14,27 @@ class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.first_layout)
+        // 为按钮添加点击事件
         button1.setOnClickListener {
-            Toast.makeText(this,"You clicked Button 1", Toast.LENGTH_SHORT).show()
+            // 添加一个Toast(提醒)
+//            Toast.makeText(this,"You clicked Button 1", Toast.LENGTH_SHORT).show()
+            // 销毁一个Activity
+//            finish()
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
         }
+    }
+    // 添加菜单
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main,menu)
+        return true
+    }
+    // 为菜单添加点击事件
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.add_item -> Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT).show()
+            R.id.remove_item -> Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
