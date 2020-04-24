8dd84dfff1474a88e5be1c17766f587397fc32e5
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-04-24 12:06:34 +0800

    Update SecondActivity.kt

--- app/src/main/java/com/example/activitytest/SecondActivity.kt
+++ app/src/main/java/com/example/activitytest/SecondActivity.kt
@@ -1,12 +1,31 @@
 package com.example.activitytest
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.second_layout.*
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second_layout)
+//        val extraData = intent.getStringExtra("extra_data")
+//        Log.d("SecondActivity","extra data is $extraData")
+        button2.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("data_return","Hello FirstActivity")
+            setResult(RESULT_OK,intent)
+            finish()
+        }
+    }
+
+    override fun onBackPressed() {
+        val intent = Intent()
+        intent.putExtra("data_return","Hello FirstActivity")
+        setResult(RESULT_OK,intent)
+        finish()
     }
 }
