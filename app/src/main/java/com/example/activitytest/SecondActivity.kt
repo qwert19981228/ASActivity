c0ce7faf54f16f29c62aa4b5eff5173249e3ab5f
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-04-25 12:11:22 +0800

    Update SecondActivity.kt

--- app/src/main/java/com/example/activitytest/SecondActivity.kt
+++ app/src/main/java/com/example/activitytest/SecondActivity.kt
@@ -7,18 +7,21 @@ import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.second_layout.*
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("SecondActivity","Task id is $taskId")
         setContentView(R.layout.second_layout)
 //        val extraData = intent.getStringExtra("extra_data")
 //        Log.d("SecondActivity","extra data is $extraData")
         button2.setOnClickListener {
-            val intent = Intent()
-            intent.putExtra("data_return","Hello FirstActivity")
-            setResult(RESULT_OK,intent)
-            finish()
+//            val intent = Intent()
+//            intent.putExtra("data_return","Hello FirstActivity")
+//            setResult(RESULT_OK,intent)
+//            finish()
+            val intent = Intent(this,ThirdActivity::class.java)
+            startActivity(intent)
         }
     }
 
@@ -27,5 +30,10 @@ class SecondActivity : AppCompatActivity() {
         intent.putExtra("data_return","Hello FirstActivity")
         setResult(RESULT_OK,intent)
         finish()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("SecondActivity","onDestroy")
     }
 }
